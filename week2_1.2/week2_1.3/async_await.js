eaa7f317514a07f0027c92ca86c1674bec12e443
Abhishek Singh <abhishekkhhsingh@gmail.com>
2025-10-03 22:42:41 +0530

    switching to web api via async await

--- week2_1.2/week2_1.3/async_await.js
+++ week2_1.2/week2_1.3/async_await.js
@@ -1,1 +1,35 @@
 
+
+// promisified in a synchronous way 
+
+// async function f()
+// {
+
+// }
+
+// not that important
+//!!!!!!!!!!!!!! async awaiit is a promisified version with clear syntax !!!!!!!!!!!! 
+function setTimeoutPromise(time) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, time)
+    })
+}
+
+async function solve() {
+    await setTimeoutPromise(1000);
+    console.log("hi");
+
+    await setTimeoutPromise(2200);
+    console.log("hello");
+
+    await setTimeoutPromise(5000);
+    console.log("hi there ");
+}
+
+
+// call the solve function 
+// ek ek kar ek  setimeout call karega 
+solve();
+
+
+console.log(" after solve function  , it will be prionted first as it is syncronous code ");
---
+++ week2_1.2/week2_1.3/async_awwai_pr.js
@@ -0,0 +1,46 @@
+
+function delay( ms){
+     return new Promise(reslove  => setTimeout (reslove))
+}
+
+
+// async function run() {
+
+//     console.log("start");
+//      await delay(3000);
+
+//      console.log( " after two seconds ");
+     
+    
+// }
+
+// run();
+// execute the function ftter declaring it 
+
+
+// fetch api  with async await 
+
+ 
+function addAsync(a, b){
+     return new Promise( resolove => {
+        setTimeout (( )=> resolove(a+b) , 3000 )
+     })
+}
+
+ async function cal(){
+  
+    console.log("Adding the number");
+     
+    // mene ise upar declare kia tha 
+     let sum = await addAsync(5,6);
+
+       console.log("result" , sum );
+    // error ?'
+
+    // mene is ebhi upar declare kia ths 
+    
+    await delay(4000);
+         console.log(" after 3 secs");
+        
+ }
+ cal();
