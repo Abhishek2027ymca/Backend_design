70a29381c3a1aaa91f1abaa75b7fee14f6ceac8a
Abhishek Singh <abhishekkhhsingh@gmail.com>
2025-11-07 14:03:25 +0530

    auth via jwt

--- week6_authentication/jwtlogic.js
+++ week6_authentication/jwtlogic.js
@@ -1,28 +1,20 @@
-const express = require("express");
-// adding an express framework
 
-/// creatinf a post  rout
+// replacing the token logic with jwt 
+const express = require("express");
+const  jwt_secret = "rnadomstringforjwtsecrettoken" ;
+
+const jwt = require("jsonwebtoken");
+
 const app = express();
-// this is a moddle ware thta will  help us to parse json data
-// into js object
+
 
 app.use(express.json());
-// ceeatig n a global varibale
 
 const users = [];
 
 // function to genreate token of 10 characters
-function generate_token() {
-  // via math.random
-  let token = "";
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  const charactersLength = characters.length;
-  for (let i = 0; i < 20; i++) {
-    token += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return token;
-}
-// creatign a random functipon to generaet token// random string
+
+
 
 app.post("/Signup", function (req, res) {
   // we will  be requiring  two thing only // as a inmmemory varibale
@@ -49,60 +41,46 @@ app.post("/Signup", function (req, res) {
   res.json({
     msg: "you are signedin successfully",
   });
-  // shjowing the user array in the console
+
   console.log(users);
 });
 
-// after  signingin
-// we habve to give  user a unique token
-
-// whenever  user hit this end point
 
 app.post("/Signin", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
-  if (!username || !password) {
-    return res.status(400).json({ msg: "username and password required" });
-  }
-
-
-
+  
   let founduser = null;
 
   for (let i = 0; i < users.length; i++) {
-    if (users[i].username === username && users[i].password === password) {
+    if (users[i].username == username && users[i].password == password) {
       founduser = users[i];
       break;
     }
   }
-
+//// IMPORTANR CHNAGE HAPPENED HERE
   if (founduser) {
-    const token = generate_token();
+    // conveting the username into a a json webtoken 
+    const token = jwt.sign({ username: username }, jwt_secret);
+// token generated above by this function 
 
-    // put that token into the founduser object
-    founduser.token = token;
-    // respond with the token
     return res.json({
       message: token,
     });
   }
 
-  // if not found, send an error
   res.status(401).json({ msg: "invalid credentials" });
 });
-// only authenticated user can access this route
 app.get("/me" , function (req, res) {
-// getting the token fomt he  header 
-// user will send us the tken  formt  the header 
-// which contains the metadata 
-// storing the token in a varibale
-  const token = req.headers.token;
+
+  const token = req.headers.token; // decode the token jwt
+  const decodeInfo = jwt.verify(token, jwt_secret);
+  const username =  decodeInfo.username ;
   const foundUser = null ;
-  //// the token i got  formt he user , i will check  it form users arrya where all the token are present 
 
  for (let i = 0; i < users.length; i++) {
-  if(users[i].token === token){
+  if(users[i].username === username){
     foundUser = users[i];
  }
 }
@@ -126,4 +104,5 @@ app.get("/me" , function (req, res) {
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
 
