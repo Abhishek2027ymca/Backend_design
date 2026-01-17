ca22a976495acc757094042948eb500519d3b868
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-01-17 22:52:01 +0530

    compass implementation

--- week7_live_mongodb/db.js
+++ week7_live_mongodb/db.js
@@ -1,7 +1,32 @@
 
 const mongoose = require("mongoose");
+ const ObjectId = mongoose.ObjectId;
 
-// importing a clas calld  schema  
+// importing a class calld  schema  
 const Schema = mongoose.Schema;
 
-const User = new Schema()
+const User = new Schema({
+     email : String,
+     password : String,
+     name : String
+
+})// defint the schema 
+// putting the dta int he table 
+
+
+const Todo = new Schema({
+    title : String ,
+    done : Boolean ,
+    userId : ObjectId 
+  
+
+
+})
+//________________________________this'users' will eb same a sin mongodb , so be specific 
+const UserModel = mongoose.model('users' , User)
+const Todomodel = mongoose.model('todos' , Todo)
+
+module.exports = {
+    UserModel : UserModel ,
+    Todomodel : Todomodel
+}
--- week7_live_mongodb/i.js
+++ week7_live_mongodb/i.js
@@ -1,16 +1,27 @@
 
 const express = require(express)
 const app = express();
+const {UserModel , TodoModel}  = require("./db")
+// skelatne for the epxress cod e
+
+// thes e2 endpoint will be non aitheticated 
 
 app.post("/signup"  , function(req, res){
        
-       
+    user.insert({
+        name: "harkirat" ,
+        password: "124dd",
+        email: " dcse@.com"
+    })
+        
 
 });
 
 app.post("/signin" ,function(req, res){
     
 });
+
+// these end potns after the  authetication 
 app.post("/todo" ,function(req, res){
 
 });
