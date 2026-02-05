fd1e499661c2cd37539a42b3eb0a10be806aff82
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-02-05 16:02:03 +0530

    useEffect and and conditional rendering

--- week_9/vite-project/src/App2.jsx
+++ week_9/vite-project/src/App2.jsx
@@ -1,5 +1,5 @@
 import { useState , useEffect } from "react";
-function App() {
+function App() {   // an app comempoentn which rendder sthe  counter compoentn itself 
   return <div>
      <Counter></Counter>
   </div>;
@@ -15,8 +15,20 @@ function Counter() {
 //     setCount(count + 1);
 //   }, 500);     // this  will amke the code break 
 // as react will rernder eveytime the component , 
-// whenevr counter value is upadted 
+// whenevr counter value is upadted  
 
+
+// WRAPPING THIS MUSH CODE IN UUSE EFFECT WILL AMKE SURE THAT NO MANY HOW MANY RENDER HAPPEMND 
+// THIS PARTICULAR CODE  WILL NOT RUN AGAINA AND AGAIN 
+console.log("counter");
+// ^^^^^ mounted component
+useEffect(function(){   //this useEffect will run only once
+    // wull take a function as an argumen, and a dependency array
+     setInterval(function () {
+     setCount( count => count + 1);
+       }, 1000);
+       console.log("mounted")   // YE ANDAR KA LOGIC BAAR BAAR RUBN NHI HOGA , DIRING RERENDER 
+}, []);
 
 
 
