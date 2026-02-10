65d6d367055e154f536957b6e043a8cd714ab2d5
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-02-10 13:12:32 +0530

    Creating  a useRef varibale

---
+++ week10.1_SPA/v/src/App1.jsx
@@ -0,0 +1,44 @@
+
+// // basic boiler plate for react router dom
+// import { React , useRef } from "react";
+// import {
+//   BrowserRouter,    
+//     Routes, } from "react-router-dom";
+
+
+
+//   function App() {
+   
+//     const inputRef = useRef(null);  // created a variable via input variable 
+
+//     function handleSubmit() {
+
+//          inputRef.current.focus();
+//          // input .current w
+
+//     }
+
+
+// return (
+//      <div>    
+//     {/* jsut like id = box  */}
+//      {/* ref = {inputRef}  is like creating a variable and storing the reference of that particular input field in that variable */} */}
+//         <input  type="text"> </input>
+//         <button onClick={handleSubmit}> submit </button>
+//      </div>)
+//   }
+
+
+
+  
+
+
+
+//    export default App;
+
+
+// //   /// ineed that cursor will focus on the first input field when the page is loaded and after clicking on submit button also it should focus on the first input field
+// //   // varoiuous ways to fetch the dom l;eemts in react
+// //     // 1. document.getElementById
+// //     // use ref in react 
+     
---
+++ week10.1_SPA/v/src/App2.jsx
@@ -0,0 +1,53 @@
+
+// basic boiler plate for react router dom
+import { React , useRef } from "react";
+import { useState } from "react";
+
+
+  function App() {
+     
+    // A clock with a start an dstop avribel 
+
+    
+    const[ curretTime , setCurrentTime] = useState(0);
+    
+    // let timer = 0  ; , i can use it as it will reintialixe its vbaue to 0 after evry stop 
+
+    const timer = useRef(); 
+    // this variable will not reintialixe its value after every stop
+    function startTimer() {
+        // return a number 
+       let valu =   setInterval(() => {
+            setCurrentTime((prev) => prev + 1);
+        }, 1000);
+        timer.current = valu; // cureent valu of time os geetings oteed 
+    }
+
+    function stopTimer() {
+        clearInterval(timer.current);
+        // here timer.current is the value of timer which is getting updated after every start
+    }
+        
+
+return (
+    <div>
+        {curretTime}
+        <button  onClick = {startTimer}> Start </button>
+        <button onClick={ stopTimer}> Stop </button>
+    </div>
+)
+  }
+
+
+  
+
+
+   export default App;
+
+
+
+
+   // if we want to start a clock , jsut call the set interval 
+   // but when we need to stop the clock at a particulur time 
+   // wheere should we store that variable so that it can run from the exact time 
+   // wehn we clikc start agaun 
