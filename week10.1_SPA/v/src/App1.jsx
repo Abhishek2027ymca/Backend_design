3d134adf184013d08c9587c4acfd16c7c0af072b
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-02-10 23:01:19 +0530

    practice

--- week10.1_SPA/v/src/App1.jsx
+++ week10.1_SPA/v/src/App1.jsx
@@ -1,32 +1,30 @@
 
-// // basic boiler plate for react router dom
-// import { React , useRef } from "react";
-// import {
-//   BrowserRouter,    
-//     Routes, } from "react-router-dom";
+// basic boiler plate for react router dom
+import { React , useRef } from "react";
+import {
+  BrowserRouter,    
+    Routes, } from "react-router-dom";
 
 
 
-//   function App() {
+  function App() {
    
-//     const inputRef = useRef(null);  // created a variable via input variable 
+    const inputRef = useRef(null);  // created a variable via input variable 
 
-//     function handleSubmit() {
+    function handleSubmit() {
 
-//          inputRef.current.focus();
-//          // input .current w
+         inputRef.current.focus();
+         // input .current w
 
-//     }
+    }
 
 
-// return (
-//      <div>    
-//     {/* jsut like id = box  */}
-//      {/* ref = {inputRef}  is like creating a variable and storing the reference of that particular input field in that variable */} */}
-//         <input  type="text"> </input>
-//         <button onClick={handleSubmit}> submit </button>
-//      </div>)
-//   }
+return (
+     <div>    
+        <input  type="text" ref={inputRef}> </input>
+        <button onClick={handleSubmit}> submit </button>
+     </div>)
+  }
 
 
 
@@ -34,11 +32,11 @@
 
 
 
-//    export default App;
+   export default App;
 
 
-// //   /// ineed that cursor will focus on the first input field when the page is loaded and after clicking on submit button also it should focus on the first input field
-// //   // varoiuous ways to fetch the dom l;eemts in react
-// //     // 1. document.getElementById
-// //     // use ref in react 
+//   /// ineed that cursor will focus on the first input field when the page is loaded and after clicking on submit button also it should focus on the first input field
+//   // varoiuous ways to fetch the dom l;eemts in react
+//     // 1. document.getElementById
+//     // use ref in react 
      
