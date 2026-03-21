b52f14371d8e24763689211d526a14b0e6a56776
Abhishek Singh <abhishekkhhsingh@gmail.com>
2026-03-21 16:54:00 +0530

    Pick_API

--- week14.2_advance/src/i.ts
+++ week14.2_advance/src/i.ts
@@ -4,11 +4,35 @@ interface User{
 
 name : string ;
 age : number ;
+email : string;
+password : string ;
+
 
 };
 
-function sumOfage (user1 : User , user2: User){
-    return user1.age +user2.age ;
+
+// a funciton to update the user in databse 
+// we can change age , email . password 
+interface UpdatedProps {
+    name : string ;
+    age? : number ;
+email? : string;
 
 }
-sumOfage({name : "Abhishek" , age : 22} , {name : "Anshul" , age : 21}) 
+
+// why this is is not good 
+// how to make upfdateprop a subset of  user interfac e
+// pullin some [prop form ] user interface
+
+
+// we use a generics 
+// from user intrfc , pick omnlyu name , age , email with their espectivbe props 
+
+type UpdatePropss = Pick<User, 'name' | 'age' | 'email'>;
+
+function updateUser( updatedProps : UpdatePropss )  {
+
+// hit the db to update the user 
+
+}
+//puick uiois used here to create a new type that only has the properties we want to update in the database
